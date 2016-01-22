4ed74af26281734d1e584a6d7eced00e96407279
YoungGer <771657815@qq.com>
2016-01-22 15:19:38 +0800

    网站定稿16_01_22

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         continuousVertical: true, //循环演示
-        anchors: ['page1', 'page2', 'page3', 'page4'],
-        menu: '#menu', //绑定菜单
+        // anchors: ['page1', 'page2', 'page3', 'page4'],
+        // menu: '#menu', //绑定菜单
         'navigation': true, //导航
     });
 });
